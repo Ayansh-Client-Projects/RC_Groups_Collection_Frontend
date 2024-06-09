da6fe8782a755072898cf88f9b58d2a59437d0e0
jayshreee10 <2001109280_ee@pmec.ac.in>
2024-06-09 16:18:08 +0530

    App Ui

---
+++ src/Pages/SignIn.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import SigninLogo from "../assets/Signinlogo.svg";
+import { useMediaQuery } from "@mui/material";
+function SignIn() {
+  const Mq = {
+    sm: useMediaQuery("(max-width:600px)"),
+    lg: useMediaQuery("(min-width:1001px)"),
+  };
+
+  return (
+    <div 
+      style={{
+        height: "100vh",
+        width: "100vw",
+        background: "white",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection:Mq.sm ? "column" :"row" ,
+        
+      }}
+    >
+      <img src={SigninLogo} style={{ height: "50%", width: "50%" }} />
+      <div
+        className="Container"
+        style={{
+          // background:"black",
+          height: "30vh",
+          width: "50vw",
+          display: "flex",
+          alignItems: "center",
+          flexDirection: "column",
+          justifyContent: "space-around",
+        }}
+      >
+        <TextField
+          id="outlined-basic"
+          label="Username"
+          variant="outlined"
+          style= {{ width: Mq.sm ? "70vw" :"23vw" }}
+        />
+        <TextField
+          id="password"
+          hintText="Password"
+          floatingLabelText="Password"
+          type="password"
+          label="Password"
+          style= {{ width: Mq.sm ? "70vw" :"23vw" }}
+        ></TextField>
+
+        <Button variant="contained"  style= {{ width: Mq.sm ? "70vw" :"23vw" }}>
+          Sign in
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default SignIn;
