0a0fcc06190dfa203a74f9c7a660a8c5b2c1e8e0
jayshreee10 <2001109280_ee@pmec.ac.in>
2024-06-10 13:10:22 +0530

    Error Page Ui

--- src/Pages/Error404.jsx
+++ src/Pages/Error404.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function Error404() {
+  const navigate = useNavigate();
+  function navigateToSignIn() {
+    navigate("/signin");
+  }
   return (
-    <div>Error404</div>
-  )
+    <div
+      style={{
+        backgroundColor: "white",
+        height: "100vh",
+        width: "100vw",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+        fontWeight: "600",
+      }}
+    >
+      <img
+        src="src/assets/Error.svg"
+        style={{ height: "50vh", width: "50vw" }}
+      />
+      <p style={{ color: "black" }}>
+        {" "}
+        OOPS!! Go to{" "}
+        <span
+          onClick={() => {
+            navigateToSignIn();
+          }}
+          style={{ color: "blue" }}
+        >
+          Login
+        </span>
+      </p>
+    </div>
+  );
 }
 
-export default Error404
+export default Error404;
